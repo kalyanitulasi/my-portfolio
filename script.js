a13778a385fda9342475bd1eaac68dc3b5dd557c
kalyanitulasi <kalyanitulasi261@gmail.com>
2026-03-28 14:13:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function family() {
+    window.location.href = "family.html"
+}
+
+function maternity() {
+    window.location.href = "maternity.html"
+}
+
+function corporate(){
+    window.location.href="corporate.html"
+}
+
